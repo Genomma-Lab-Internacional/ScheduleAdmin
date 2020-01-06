4bb296950032c51b70ad8443e15ee5e73e40ef25
caortizpacheco <ca.ortiz.pacheco@gmail.com>
2020-01-06 16:47:32 -0600

    almost end

--- serverless/handler.js
+++ serverless/handler.js
@@ -48,3 +48,46 @@ module.exports.verifyDateRoom = (event,context,callback) => {
   })
   
 }
+
+module.exports.createDateRoom = (event,context,callback) => {
+  let body = JSON.parse(event.body)
+  
+  console.log(body)
+  console.log(Object.keys(body)[1])
+
+  let params = {
+    TableName : 'DateRoom',
+    Key: {
+      date : body.date,
+      user: "cortiz"
+    },
+    ExpressionAttributeNames: {
+      "#R" : Object.keys(body)[1],
+      "#H" : Object.values(body)[1]
+    },
+    ExpressionAttributeValues: {
+      ":b" : true
+    },
+    UpdateExpression: `SET #R.#H = :b`  
+  };
+  
+  DDB.update(params, (err, data) => {
+    if (err) {
+      console.log("ERROR",err)
+      callback({
+        statusCode: 400,
+        body: JSON.stringify(err),
+        headers: {
+          "Access-Control-Allow-Origin":"*"
+      }})
+    } else {
+      console.log("DONE",data)
+      callback(null,{
+        statusCode: 200,
+        body: JSON.stringify(data),
+        headers: {
+          "Access-Control-Allow-Origin":"*"
+      }})
+    }
+  })
+}
--- src/Components/ScheduleRoom.js
+++ src/Components/ScheduleRoom.js
@@ -7,21 +7,46 @@ export default function ScheduleRoom() {
   const [ date, setDate ] = useState({})
   const [ hour, setHour ] = useState({})
   const [ rooms, setRooms ] = useState({})
-  const [ room1, setRoom1] = useState({"800":false})
+  const [ room1, setRoom1] = useState({})
+  const [ room2, setRoom2] = useState({})
+  const [ room3, setRoom3] = useState({})
+  const [ room4, setRoom4] = useState({})
+  const [ room5, setRoom5] = useState({})
 
   const selectDate = (e) => {
     e.persist()
     date[e.target.name] = e.target.value
     axios.post("https://8ur4vxt8u5.execute-api.us-east-1.amazonaws.com/dev/miscelanea/verify-date-room",date)
       .then( success => {
-        let room1 = success.data.Item.room1
-        for (let i=0; i<room1.length; i++) {
-            console.log(room1[i])
-            if(room1[1]) {
-              setRoom1({800:true})
-            }
-          // setRoom1({800:result[i][0]})
-        }
+        setRoom1({ 
+          800:success.data.Item.room1[800],
+          900:success.data.Item.room1[900],
+          1000:success.data.Item.room1[1000],
+          1100:success.data.Item.room1[1100],
+          1200:success.data.Item.room1[1200],
+          1300:success.data.Item.room1[1300],
+          1400:success.data.Item.room1[1400],
+          1500:success.data.Item.room1[1500],
+          1600:success.data.Item.room1[1200],
+          1700:success.data.Item.room1[1300],
+          1800:success.data.Item.room1[1400],
+          1900:success.data.Item.room1[1500] 
+         })
+         setRoom2({ 
+          800:success.data.Item.room2[800],
+          900:success.data.Item.room2[900],
+          1000:success.data.Item.room2[1000],
+          1100:success.data.Item.room2[1100],
+          1200:success.data.Item.room2[1200],
+          1300:success.data.Item.room2[1300],
+          1400:success.data.Item.room2[1400],
+          1500:success.data.Item.room2[1500],
+          1600:success.data.Item.room2[1200],
+          1700:success.data.Item.room2[1300],
+          1800:success.data.Item.room2[1400],
+          1900:success.data.Item.room2[1500] 
+         })
+
       })
       .catch( error => console.log(error) ) 
   }
@@ -33,7 +58,10 @@ export default function ScheduleRoom() {
   
 
   const confirmRoom = () => {
-    console.log(hour)
+    let test= {...date,...hour}
+    axios.post("https://8ur4vxt8u5.execute-api.us-east-1.amazonaws.com/dev/miscelanea/create-date-room",test)
+      .then( success => console.log(success) )
+      .catch( error => console.log(error) )
   }
 
   return (
@@ -41,17 +69,47 @@ export default function ScheduleRoom() {
       <h2>Selecciona una Fecha</h2>
       <input onChange={selectDate} type="date" name="date"/>
       <div>
-        <h2>Sala 1</h2>
+        <h3>Sala 1</h3>
         <div>
-          {schedule.map( s => <button onClick={selectHour} type="text" name="room1"  value={s} disabled={room1[s]===true ? true : false}>{s}</button>)}
+          {schedule.map( s => <button onClick={selectHour} type="text" name="room1" value={s} disabled={room1[s] === true ? true : false}>{s}</button>)}
           <button onClick={confirmRoom}>Confirmar Sala</button>
         </div>
       </div>
+      <div>
+        <h3>Sala 2</h3>
+        <div>
+          {schedule.map( s => <button onClick={selectHour} type="text" name="room2" value={s} disabled={room2[s] === true ? true : false}>{s}</button>)}
+          <button onClick={confirmRoom}>Confirmar Sala</button>
+        </div>
+      </div>
+      <div>
+        <h3>Sala 3</h3>
+        <div>
+          {schedule.map( s => <button onClick={selectHour} type="text" name="room3" value={s} disabled={room3[s] === true ? true : false}>{s}</button>)}
+          <button onClick={confirmRoom}>Confirmar Sala</button>
+        </div>
+      </div>
+      <div>
+        <h3>Sala 4</h3>
+        <div>
+          {schedule.map( s => <button onClick={selectHour} type="text" name="room4" value={s} disabled={room4[s] === true ? true : false}>{s}</button>)}
+          <button onClick={confirmRoom}>Confirmar Sala</button>
+        </div>
+      </div>
+      <div>
+        <h3>Sala 5</h3>
+        <div>
+          {schedule.map( s => <button onClick={selectHour} type="text" name="room5" value={s} disabled={room5[s] === true ? true : false}>{s}</button>)}
+          <button onClick={confirmRoom}>Confirmar Sala</button>
+        </div>
+      </div>
+   
+
       {/* {room.map( r => (
         <div>
           <h3>Sala {r}</h3>
             <div>
-              {schedule.map( s => <button onClick={selectHour} type="text" name=`room-${r}`  value={s} disabled={rooms["room1"] ? true : false}> {s} </button>)}
+              {schedule.map( s => <button onClick={selectHour} type="text" name={`room${r}`} value={s} disabled={`room${r}[${s}]`=== true ? true : false}> {s} </button>)}
               <br/>
               <button onClick={confirmRoom}>Confirmar Sala</button>
             </div>
